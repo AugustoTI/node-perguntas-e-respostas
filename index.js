0d5d8e0025fc39e90001151b86e0371aea62562b
AugustoTI <augustocesarti2021@gmail.com>
2022-10-27 23:25:14 -0300

    initial project structure

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const server = express()
+const { resolve } = require('path')
+
+server.set('view engine', 'ejs')
+server.set('views', resolve(__dirname, 'src', 'views'))
+
+server.get('/', (req, res) => {
+  res.render('index')
+})
+
+server.listen(3000, () => {
+  console.log('Servidor rodando na URL: http://localhost:3000')
+})
